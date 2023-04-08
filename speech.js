1344dc1430561689547bd3ba20b7756f83a23067
PGSCOM <69808296+PGSCOM@users.noreply.github.com>
2023-04-08 15:10:55 +0200

    Update speech.js

--- speech.js
+++ speech.js
@@ -1,13 +1,13 @@
 // Configuración de la voz del robot
 
-const synth = window.speechSynthesis
+const synth = window.speechSynthesis;
 
 const textToSpeech = (string) => {
-  const voice = new SpeechSynthesisUtterance(string)
-  voice.text = string
-  voice.lang = 'es-ES'
-  voice.volume = 1
-  voice.rate = 1
-  voice.pitch = 1 // Puede ser 0, 1 o 2
-  synth.speak(voice)
+  let voice = new SpeechSynthesisUtterance(string);
+  voice.text = string;
+  voice.lang = "es-ES";
+  voice.volume = 1;
+  voice.rate = 1;
+  voice.pitch = 1; // Puede ser 0, 1 o 2
+  synth.speak(voice);
 }
